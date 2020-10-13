9bb664e94a3a268180884f7478d4d48dc7fbfc83
Oran Agra <oran@redislabs.com>
2020-10-13 10:15:04 +0300

    clients eviction wip

--- src/networking.c
+++ src/networking.c
@@ -1580,6 +1580,7 @@ int writeToClient(client *c, int handler_installed) {
             return C_ERR;
         }
     }
+    updateClientMemUsage(c);
     return C_OK;
 }
 
@@ -3733,14 +3734,17 @@ int clientEvictionCheckLimit() {
 }
 
 void clientsEviction() {
+    raxIterator ri;
     if (!clientEvictionCheckLimit())
         return;
 
-    raxIterator ri;
     raxStart(&ri,server.client_eviction_pull);
     raxSeek(&ri,"$",NULL,0);
     while (raxPrev(&ri)) {
         client *best = ri.data;
+        sds ci = catClientInfoString(sdsempty(),best);
+        serverLog(LL_NOTICE, "Evicting client: %s", ci);
+        sdsfree(ci);
         freeClient(best);
         server.stat_evictedclients++;
         if (!clientEvictionCheckLimit())
